9288f5820a52db39654fd1c15f664d4b7d48a2dd
pTina <gaeun2937@gmail.com>
2019-05-08 16:56:31 +0900

    인텐트 액티비티전환

--- src/main/java/com/example/copy_instagram/MainActivity.kt
+++ src/main/java/com/example/copy_instagram/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.copy_instagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btMainProfileEdit.setOnClickListener {
+            val nextIntent = Intent(this, SecondActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
---
+++ src/main/java/com/example/copy_instagram/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.copy_instagram
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        btSecondDone.setOnClickListener {
+            val nextIntent = Intent(this, MainActivity::class.java)
+            startActivity(nextIntent)
+        }
+    }
+}
